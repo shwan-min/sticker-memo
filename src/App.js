97bb97ba30bd10fd7ec973995cedebbd8033198f
Shwan.Min <ufo3989@gmail.com>
2024-02-23 20:38:18 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { observer } from 'mobx-react';
+import Memo from './Memo/Memo';
+import { Add } from '@mui/icons-material';
+import { useCallback } from 'react';
 
-function App() {
+function App({store}) {
+  const AddMemo = useCallback(() => store.addMemo(), [store]);
+  const Edit = useCallback((id, content) => store.editMemo(id, content), [store]);
+  const SetSize = useCallback((id, width, height) => store.setSize(id, width, height),[store])
+  const SetPosition = useCallback((id, x, y) => store.setPosition(id, x, y), [store]);
+  const Delete = useCallback((id) => {store.removeMemo(id)},[store])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+      store.memos.map((memo) => 
+        <Memo 
+          key={memo.id}
+          item={memo}
+          Edit={Edit}
+          SetSize={SetSize}
+          SetPosition={SetPosition}
+          Delete={Delete}
+        />)
+    }
+    <Add 
+      sx={{
+        float:'right',
+        backgroundColor: '#e4e4e4',
+        borderRadius: 2,
+        cursor: 'pointer',
+        fontSize: 30,
+        border: '1px solid black' }}
+      onClick={AddMemo}
+    />
+    </>
   );
 }
 
-export default App;
+export default observer(App);
